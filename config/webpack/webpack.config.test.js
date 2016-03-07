88b2bf73745eb02c99a972015a50b59f6a0519fe
Lauren <lauren.eastridge@formidablelabs.com>
2016-03-06 16:52:04 -0800

    fix enzyme + sinon + webpack issue

--- config/webpack/webpack.config.test.js
+++ config/webpack/webpack.config.test.js
@@ -8,6 +8,7 @@ var prodCfg = require("./webpack.config");
 
 // Replace with `__dirname` if using in project root.
 var ROOT = process.cwd();
+var _ = require("lodash"); // devDependency
 
 module.exports = {
   cache: true,
@@ -19,10 +20,23 @@ module.exports = {
   },
   resolve: _.merge({}, prodCfg.resolve, {
     alias: {
+      // enzyme webpack issue https://github.com/airbnb/enzyme/issues/47
+       sinon: "node_modules/sinon/pkg/sinon.js",
       // Allow root import of `src/FOO` from ROOT/src.
       src: path.join(ROOT, "src")
     }
   }),
-  module: prodCfg.module,
+  // enzyme webpack issue https://github.com/airbnb/enzyme/issues/47
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  module: _.assign({}, prodCfg.module, {
+    // enzyme webpack issue https://github.com/airbnb/enzyme/issues/47
+    noParse: [
+      /\/sinon\.js/
+    ]
+  }),
   devtool: "source-map"
 };
