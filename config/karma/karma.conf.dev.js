ca3fea8b2a96c840148aada46eef50d1709f3bd8
boygirl <lauren.eastridge@formidable.com>
2017-11-02 20:53:55 -0700

    lint

--- config/karma/karma.conf.dev.js
+++ config/karma/karma.conf.dev.js
@@ -21,8 +21,8 @@ module.exports = function (config) {
       archDevRequire.resolve("sinon/pkg/sinon"),
 
       // Polyfills for PhantomJS in React 16.
-      archDevRequire.resolve('core-js/es6/map'),
-      archDevRequire.resolve('core-js/es6/set'),
+      archDevRequire.resolve("core-js/es6/map"),
+      archDevRequire.resolve("core-js/es6/set"),
 
       // Test bundle (must be created via `npm run dev|hot|server-test`)
       "http://127.0.0.1:3001/assets/main.js"
